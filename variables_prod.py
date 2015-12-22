4d382ecc211186bb276a23cefe03f0065ee42453
Peter Glas <pglas@anwb.nl>
2015-12-22 13:32:05 +0100

    Use Jira api to speed things up. Delay is nolonger necessary. Added tag All. Moved username and password variables to variables_prod.py. Also place to set jiraboard url.

--- variables_prod.py
+++ variables_prod.py
@@ -1,2 +1,11 @@
+USERNAME = 'YOUR-JIRA-ACCOUNT'
+PASSWORD = 'YOUR-JIRA-PASSWORD'
+JIRABOARD = '/jira/secure/RapidBoard.jspa?rapidView=132&view=planning.nodetail'     #Daltons
+#/jira/secure/RapidBoard.jspa?rapidView=132&view=planning.nodetail     #Daltons
+#/jira/secure/RapidBoard.jspa?rapidView=108&view=planning.nodetail       #Verzekeren
+JIRABOARDNAME =	'Media en POI, Vakantie, Vrije Tijd'      #Daltons
+#'Media en POI, Vakantie, Vrije Tijd'      #Daltons
+#'ANWB Verzekeren'      #Verzekeren
+
 SERVER = 'https://projprod'
-__all__ = ['SERVER']
+__all__ = ['JIRABOARDNAME', 'JIRABOARD', 'SERVER', 'USERNAME', 'PASSWORD']
